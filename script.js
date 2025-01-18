43c162e19ff01847e80b95c48284cb4d1f670176
Mahir Durna <159443825+DurnaMa@users.noreply.github.com>
2025-01-18 17:19:36 +0100

    showQuestion

--- script.js
+++ script.js
@@ -46,8 +46,51 @@ let questions = [
     answer_5: 'Es formatiert den Text',
     right_answer: 2,
   },
+  {
+    question: 'Was ist das Hauptziel von HTML?',
+    answer_1: 'Datenbankmanagement',
+    answer_2: 'Gestaltung von Webseiten',
+    answer_3: 'Strukturierung von Inhalten',
+    answer_4: 'Programmierung von Apps',
+    answer_5: 'Netzwerksicherheit',
+    right_answer: 3,
+  },
+  {
+    question: 'Welche Eigenschaft in CSS wird verwendet, um die Textfarbe zu ändern?',
+    answer_1: 'background-color',
+    answer_2: 'font-size',
+    answer_3: 'text-align',
+    answer_4: 'color',
+    answer_5: 'display',
+    right_answer: 4,
+  },
+  {
+    question: 'Welche HTML-Tags werden verwendet, um eine Tabelle zu erstellen?',
+    answer_1: '<table>, <tr>, <td>',
+    answer_2: '<div>, <span>, <p>',
+    answer_3: '<ul>, <li>, <ol>',
+    answer_4: '<header>, <footer>, <main>',
+    answer_5: '<form>, <input>, <label>',
+    right_answer: 1,
+  }
 ];
 
 function init(){
+  document.getElementById("all-questions").innerHTML = questions.length;
+  showQuestion();
+}
+let currentQuestion = 0;
+
+function showQuestion() {
+  let question = questions[currentQuestion];
+  document.getElementById('questiontext').innerHTML = question['question'];
+  document.getElementById('answer_1').innerHTML = question['answer_1'];
+  document.getElementById('answer_2').innerHTML = question['answer_2'];
+  document.getElementById('answer_3').innerHTML = question['answer_3'];
+  document.getElementById('answer_4').innerHTML = question['answer_4'];
+  document.getElementById('answer_5').innerHTML = question['answer_5'];
+}
+
+function answer(slection) {
 
 }
