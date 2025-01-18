6c65f5f1c52b56efc63ac4265bacadc2758b3e58
Mahir Durna <159443825+DurnaMa@users.noreply.github.com>
2025-01-18 16:13:41 +0100

    questions

--- script.js
+++ script.js
@@ -1,1 +1,53 @@
-bad
+let questions = [
+  {
+    question: 'Was ist das Hauptziel von HTML ?',
+    answer_1: 'Datenbankmanagement',
+    answer_2: 'Gestaltung von Webseiten',
+    answer_3: 'Strukturierung von Inhalten',
+    answer_4: 'Programmierung von Apps',
+    answer_5: 'Netzwerksicherheit',
+    right_answer: 3,
+  },
+  {
+    question: 'Was bewirkt das CSS-Attribut "display: none"?',
+    answer_1: 'Das Element wird zentriert',
+    answer_2: 'Das Element wird versteckt und nimmt keinen Platz ein',
+    answer_3: 'Das Element wird nicht bearbeitbar',
+    answer_4: 'Das Element wird hervorgehoben',
+    answer_5: 'Das Element wird in einen Block umgewandelt',
+    right_answer: 2,
+  },
+  {
+    question:
+      'Welche HTML-Tags werden verwendet, um eine ungeordnete Liste zu erstellen?',
+    answer_1: '<ol>',
+    answer_2: '<ul>',
+    answer_3: '<list>',
+    answer_4: '<item>',
+    answer_5: '<li>',
+    right_answer: 2,
+  },
+  {
+    question:
+      'Welches der folgenden CSS-Attribute wird verwendet, um den Text auszurichten?',
+    answer_1: 'text-align',
+    answer_2: 'font-size',
+    answer_3: 'padding',
+    answer_4: 'color',
+    answer_5: 'margin',
+    right_answer: 1,
+  },
+  {
+    question: 'Was macht das HTML-Tag <a>?',
+    answer_1: 'Es erstellt ein Bild',
+    answer_2: 'Es erstellt einen Link',
+    answer_3: 'Es definiert eine Überschrift',
+    answer_4: 'Es erstellt eine Tabelle',
+    answer_5: 'Es formatiert den Text',
+    right_answer: 2,
+  },
+];
+
+function init(){
+
+}
